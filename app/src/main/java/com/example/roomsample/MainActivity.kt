f72c9962aa7824fdeaccf0b7f9c3688809f05635
Ivan Kuznetsov <kyzaiv1@yandex.ru>
2021-12-28 14:45:30 +1000

    sad

--- app/src/main/java/com/example/roomsample/MainActivity.kt
+++ app/src/main/java/com/example/roomsample/MainActivity.kt
@@ -16,18 +16,18 @@ class MainActivity : AppCompatActivity() {
         Executors.newSingleThreadExecutor().execute {
             val t = AppDatabase
                 .getInstance(applicationContext)
-            val genre = Genre(1,"classic")
-            val authors = arrayOf(Author(1,"Pushkin","Alexander","Sergeevich"),
-                Author(2,"Fet","Aphanasiy","Aphanasievich"))
-            val books = arrayOf(
-                Book(1,"Pushkins book",1),
-                Book(2,"Fets book",1))
-            t.genreDao().insertGenre(genre)
-            t.authorDao().insertAuthors(authors[0],authors[1])
-            t.bookDao().insertBooks(books[0],books[1])
-            val a1 = AuthorBookRef(1, 1)
-            val a2 = AuthorBookRef(2, 2)
-            t.authorDao().addBooks(a1,a2)
+//            val genre = Genre(1,"classic")
+//            val authors = arrayOf(Author(1,"Pushkin","Alexander","Sergeevich"),
+//                Author(2,"Fet","Aphanasiy","Aphanasievich"))
+//            val books = arrayOf(
+//                Book(1,"Pushkins book",1),
+//                Book(2,"Fets book",1))
+//            t.genreDao().insertGenre(genre)
+//            t.authorDao().insertAuthors(authors[0],authors[1])
+//            t.bookDao().insertBooks(books[0],books[1])
+//            val a1 = AuthorBookRef(1, 1)
+//            val a2 = AuthorBookRef(2, 2)
+//            t.authorDao().addBooks(a1,a2)
             t.authorDao().getAllAuthorsWithBooks().forEach {
                 Log.d("AUTHOR", it.author.firstname+" "+it.books.size)
             }
